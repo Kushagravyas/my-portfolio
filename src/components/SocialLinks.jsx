57095c395f429f8873ce201d18ef340841973632
kushagra vyas <kushagravyas1122@gmail.com>
2024-09-18 03:42:28 +0530

    Resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -41,7 +41,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: 'https://linkedin.com',
+      href: 'Kushagra_resume.pdf',
       style: 'rounded-br-md',
       download: true,
     }
