b63ea4b2e8510764883f39f03e1be94dc34c7c01
kushagra vyas <kushagravyas1122@gmail.com>
2024-09-18 03:01:38 +0530

    my portfolio web-site

---
+++ src/components/About.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-scren bg-gradient-to-b from-gray-800 to-black text-white">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-4">
+            <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+        </div>
+        <p className="text-2xl mt-2"> I am a highly skilled professional in the field of full-stack development.
+           Strong understanding of database technologies and development tools , with an ability to learn and adapt to new technologies . 
+          Eager to work collaboratively with the team to design and develop user friendly web application.
+        </p>
+            
+      </div>
+    </div>
+  )
+}
+
+export default About
