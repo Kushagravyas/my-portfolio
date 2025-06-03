44ff322744fcb532d5d643656c3e7278be8afd72
kushagra vyas <kushagravyas1122@gmail.com>
2025-06-03 12:18:28 +0530

    Links changed

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -45,7 +45,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "Kushagra_resume.pdf",
+      href: "KushagraVyasResume.pdf",
       style: "rounded-br-md",
       download: true,
       gradient: "from-green-500 to-emerald-600",
